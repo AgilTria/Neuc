8c8ebe576831576268409d299b6271aff5efd7bf
Isekai <isekai@pop-os.localdomain>
2022-11-16 17:44:29 +0700

    update migration Acara dan Sertifikat

---
+++ database/migrations/2022_11_16_102722_create_acaras_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acaras', function (Blueprint $table) {
+            $table->bigIncrements('acara_id')->primary();
+            $table->string('nama_acara');
+            $table->string('tanggal_acara');
+            $table->bigInteger('instansi_id');
+            $table->string('template_sertifikat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('acaras');
+    }
+};
